7146a7551d6a0642cd682d932654db5d1cff19e2
Nawar <abdonawar80@gmail.com>
2021-09-01 02:45:23 +0200

    Q53E DivideAndConquer

---
+++ Problems/(53) Maximum Subarray.py
@@ -0,0 +1,47 @@
+# Question Link
+# https://leetcode.com/problems/maximum-subarray/
+# Description
+# Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+# A subarray is a contiguous part of an array.
+
+
+
+
+
+
+
+# ------------------ DIVIDE AND CONQUER SOULTION FOR PRACTIVE ------------------------#
+class Solution:
+    
+    def evaluateCrossingSum(self,array,left, mid,right):
+
+        sum = 0
+        # Left
+        sumL = -1000000000000
+        for i in range(mid, left-1 , -1):
+            sum = sum + array[i]
+            sumL = max(sum , sumL)
+    
+        # Right
+        sumR = -100000000000000000
+        sum = 0
+        for i in range(mid+1 ,right + 1):
+            sum = sum + array[i]
+            sumR = max(sum , sumR)
+            
+        return max(sumL,sumR , sumL+sumR)
+    def evaluateSumRL(self,array,left,right):
+        if left == right :
+            return array[0]
+        mid = (right+left) //2
+        
+        
+        return max(self.evaluateSumRL(array , left , mid) , 
+                   self.evaluateSumRL(array,mid+1 , right) ,
+                   self.evaluateCrossingSum(array, left, mid, right))
+    def maxSubArray(self, nums: List[int]):
+                   
+        return self.evaluateSumRL(nums , 0 , len(nums)-1)
+    
+    
+# ---------------- There is a better O(n) option --------- CHECK Kadane's algo on YT -------------#
