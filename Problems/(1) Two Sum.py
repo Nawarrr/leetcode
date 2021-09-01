8572ab11ed52804034431bc8bf5da788b690798d
Nawar <abdonawar80@gmail.com>
2021-09-01 13:16:27 +0200

    Q1E Hashing Answer

--- Problems/(1) Two Sum.py
+++ Problems/(1) Two Sum.py
@@ -24,3 +24,18 @@ class Solution(object):
                 elif nums.count(num) > 1:
                      output.append(i)
         return output        
+
+#------------------- HASH ANSWER ------------- O(n)------- optimal solution ------------#
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash = {}
+        for index,value in enumerate(nums) :
+            if hash.get(str(value)):
+                return [index , nums.index(target - value)]
+            else:
+                hash[str(target - value)] = True
+            
+# There is Also another soultion using binary search 
+# Sort in O(nLogn) Loop over and use binary search to search for target-i in O(nLogn)
