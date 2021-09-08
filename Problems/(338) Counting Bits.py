bdf072a8f5720a744a19c916ab2701807d5a3f0f
Nawar <abdonawar80@gmail.com>
2021-09-08 03:47:42 +0200

    Q338E Bit-Manipulation_DP

---
+++ Problems/(338) Counting Bits.py
@@ -0,0 +1,23 @@
+class Solution:
+    #---------------------------------------- O(n Log n) ----------------------------------------# 
+    def countBits(self, n: int) -> List[int]:
+        nums = []
+        for i in range(n+1):
+            counter = 0
+            while i != 0:
+                one = i % 2
+                if one : counter +=1
+                i = i //2
+            nums.append(counter)
+        return nums
+    #----------------------------------------     O(n)     ----------------------------------------# Using  DP 
+    def countBits(self, n: int) -> List[int]:
+        import math
+        dp = [0]
+        power = 0
+        for i in range(1,n+1):
+            if math.log(i,2).is_integer()  :
+                power = i
+
+            dp.append(1 + dp[i-power]) 
+        return dp
