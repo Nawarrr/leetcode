bf34c9e8330915f9d42d51adebbc6ee98ce23d1c
Nawar <abdonawar80@gmail.com>
2021-09-10 19:30:54 +0200

    Q104E BT

--- Problems/(102) Binary Tree Level Order Traversal.py
+++ Problems/(102) Binary Tree Level Order Traversal.py
@@ -1,3 +1,12 @@
+# Question Link
+# https://leetcode.com/problems/binary-tree-level-order-traversal/
+
+
+#Description
+'''
+Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+'''
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
