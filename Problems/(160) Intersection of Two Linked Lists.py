5ebdd63f998fb15a2ce425d6e844415921ca07f9
Nawar <abdonawar80@gmail.com>
2021-09-10 04:49:32 +0200

    Q160E Linked List

---
+++ Problems/(160) Intersection of Two Linked Lists.py
@@ -0,0 +1,56 @@
+    # Definition for singly-linked list.
+    # class ListNode:
+    #     def __init__(self, x):
+    #         self.val = x
+    #         self.next = None
+
+    class Solution:
+        #--------------------------------------- O(N) time , O(n) space -----------------------------------# 
+        def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+            hash = {}
+            while headA:
+                hash[headA] = True
+                headA = headA.next
+            while headB :
+                if hash.get(headB):
+                    return headB
+                headB = headB.next
+        #--------------------------------------- O(N) time , O(1) space -----------------------------------# 
+        def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+            currA,currB = headA,headB
+            countA,countB = 0,0
+            # Counting Both of them
+            while currA:
+                countA += 1
+                currA = currA.next
+            
+            while currB:
+                countB += 1
+                currB = currB.next
+                
+            diff = countA-countB
+            # Moving the Diffrence (Unclean AF way to do it, I can do better but fuck it who cares)
+            if countA > countB :
+                diff = countA-countB
+                currA = headA
+                for i in range(diff):
+
+                    currA = currA.next
+                currB = headB
+                
+            else:
+                diff = countB-countA
+                currB =headB
+                for i in range(diff):
+
+                    currB = currB.next
+                currA = headA
+            # Moving them together 
+            while currA and currB:
+                if currA == currB:
+                    return currA
+                currA = currA.next
+                currB = currB.next
+            
+            #------------ There is a better soultion where you traverse with both of them and when they get to null you switch them to the other one,
+            #  so they both find out each other ----------
