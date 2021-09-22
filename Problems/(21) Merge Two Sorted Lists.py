ad3226eb5155982699a393231c7c407371c62cef
Nawar <abdonawar80@gmail.com>
2021-09-22 13:52:52 +0200

    Q21E Recursive Approach

--- Problems/(21) Merge Two Sorted Lists.py
+++ Problems/(21) Merge Two Sorted Lists.py
@@ -15,6 +15,7 @@ Merge two sorted linked lists and return it as a sorted list. The list should be
 #         self.val = val
 #         self.next = next
 class Solution:
+    # -------------------  O(n+m) ---------------------------#
     def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         if not l1: return l2
         if not l2: return l1
@@ -34,5 +35,24 @@ class Solution:
             
         return head.next
  
- # -------------------  O(n+m) ---------------------------#
+    # ----------------------------------- Recursive Approach---------------------------------------------- #
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        def merge(l1 , l2, head):
+            if not l1 and not l2:
+                return 
+            if not l1: 
+                head.next = l2
+                return 
+            if not l2: 
+                head.next = l1
+                return
+            if l1.val <= l2.val:
+                head.next = l1
+                merge(l1.next , l2 , head.next)
+            else:
+                head.next = l2
+                merge(l1 , l2.next , head.next)
+        head = curr = ListNode()
+        merge(l1,l2,head)
+        return curr.next
  
