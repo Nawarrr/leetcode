46f50fa09c3764bfb555b911e9dc86b650185c52
Nawar <abdonawar80@gmail.com>
2021-09-27 13:08:15 +0200

    Q155E Design (Added Linked List Soultion)

--- Problems/(155) Min Stack.py
+++ Problems/(155) Min Stack.py
@@ -14,7 +14,7 @@ void pop() removes the element on the top of the stack.
 int top() gets the top element of the stack.
 int getMin() retrieves the minimum element in the stack.
 '''
-
+# ------------------------------------------------------ Using A Hash ---------------------------------------#
 class MinStack:
 
     def __init__(self):
@@ -40,6 +40,49 @@ class MinStack:
 
     def getMin(self) -> int:
         return self.minHash[len(self.stack)-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+
+
+
+# --------------------------------------- Using a Linked List -------------------------------------------#
+class ListNode:
+    def __init__(self,val = 0 ,next=None):
+        self.val = val
+        self.next = next
+        
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        
+        
+
+    def push(self, val: int) -> None:
+        node = ListNode(val)
+        if len(self.stack) == 0:
+            node.next = ListNode(val)
+        else:
+            node.next = ListNode(min(val , self.stack[-1].next.val))
+        self.stack.append(node)
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1].val
+
+    def getMin(self) -> int:
+        return self.stack[-1].next.val
 
 
 # Your MinStack object will be instantiated and called as such:
