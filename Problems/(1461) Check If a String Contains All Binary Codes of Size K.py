aeb156b4e2ab006923e3c4a78532215422d330bd
Nawar <abdonawar80@gmail.com>
2022-05-31 16:20:55 +0200

    Q1461M

---
+++ Problems/(1461) Check If a String Contains All Binary Codes of Size K.py
@@ -0,0 +1,10 @@
+# Question Link
+# https://leetcode.com/problems/check-if-a-string-contains-all-binary-codes-of-size-k/
+class Solution:
+    def hasAllCodes(self, s: str, k: int) -> bool:
+        # O(n) Time O(2**K) Space
+        lookup = {}
+        for i in range(len(s)-k+1):
+            lookup[s[i:i+k]] = True
+        return len(lookup.keys()) == 2**k
+            
