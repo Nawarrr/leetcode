b75471a8874d5f3a5752538c41ff54e796f700e6
Nawar <abdonawar80@gmail.com>
2022-07-07 20:42:33 +0200

    Q1122E

---
+++ Problems/(1122) Relative Sort Array.py
@@ -0,0 +1,25 @@
+# Question Link 
+# https://leetcode.com/problems/relative-sort-array/
+
+
+class Solution:
+    # O(max(n,m)) Time O(1) Space  
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        hash_map = {}
+        
+        for i in range(len(arr2)) :
+            hash_map[arr2[i]] = i
+        
+        # Bucket Sort
+        length = len(arr2)
+        buckets = [[]for i in range(1001 + length)]
+        
+        for num in arr1 :
+            if num in hash_map :
+                buckets[hash_map[num]].append(num)
+            else:
+                buckets[length+num].append(num)
+
+        
+        return [num for bucket in buckets for num in bucket]
+       
